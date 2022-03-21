6b6ac007da71e56d31621822f96b9036c88ed0ce
ZmL <62185340+RandZmL@users.noreply.github.com>
2022-03-21 05:30:14 +0300

    v1.01

--- main.cpp
+++ main.cpp
@@ -6,21 +6,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    thread Thread1;
-    Thread1.name = "Thread_1";
-
-    thread Thread2;
-    Thread2.name = "Thread_2";
-
-    thread Thread3;
-    Thread3.name = "Thread_3";
-
     MyServer server;
-
     server.startServer();
-    Thread1.start();
-    Thread2.start();
-    Thread3.start();
 
     return a.exec();
 }
